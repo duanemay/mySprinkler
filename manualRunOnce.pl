91f838e982d511cb2c3cd449ebc9b6e5bd443309
Duane May <djmay@mayhoo.com>
2014-06-11 20:22:29 -0400

    Added program to run through the program once

---
+++ manualRunOnce.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use cycleSprinklers;
+use weatherLib;
+use dateLib;
+
+$SIG{'INT'} = 'terminationHandler';
+$SIG{'ABRT'} = 'terminationHandler';
+$SIG{'TERM'} = 'terminationHandler';
+$SIG{'SEGV'} = 'terminationHandler';
+$SIG{'SEGV'} = 'terminationHandler';
+
+my $time = time;
+print getDateString($time), " - Watering 1\n";
+$cycleSprinklers::DEBUG = 1;
+
+&cycleSprinklers(1);
+
+my $time = time;
+print getDateString($time), " - stopping\n";
+
+
+sub terminationHandler {
+  print "Termination Signal Recieved - stopping\n";
+  turnSprinklersOff();
+  exit(-10);
+}
+
+__END__
